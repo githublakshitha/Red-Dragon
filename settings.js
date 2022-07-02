1406cfb12555395532538d249b242d1e31e5e028
githublakshitha <108570146+githublakshitha@users.noreply.github.com>
2022-07-02 14:19:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,26 +19,26 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is ＤＡＲＫ ＷＩＰＥ ＢＯＴ
+My Creator is lakshithaヤ` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94761494707'] //Owner number in aive msg
+global.premium = ['+94761494707'] //Owner Number info
+global.ownernomer = '+94761494707' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'lakshithaヤ' //Owner Name
+global.botname = ' ＤＡＲＫ ＷＩＰＥ ＢＯＴ  ' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤖Watsapp bot group⚠️ //Costomize A Button Name In Alive Message
+global.btnurl = 'https://chat.whatsapp.com/E18463TyjkL85cSqkHRapm' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'Sri lanka' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'ＤＡＲＫ ＷＩＰＥ ＢＯＴ '//Bot Pacage Name
+global.author = 'ᶥakshithaヤ' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
-global.sp = '⭔' //A Charactor In Menu!
+global.sp = '❤️' //A Charactor In Menu!
 global.footer = '©_Kumuthu.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
